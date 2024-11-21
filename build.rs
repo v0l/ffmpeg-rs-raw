d783291f74cf1721fe240f5f7e24c0a17f63daaa
kieran <kieran@harkin.me>
2024-11-21 10:23:19 +0000

    fix: try export env

--- build.rs
+++ build.rs
@@ -4,15 +4,13 @@ fn main() {
     // re-export ffmpeg-sys-the-third features
     // https://github.com/FFmpeg/FFmpeg/blob/master/doc/APIchanges
     for (name, _value) in env::vars() {
-        if name.starts_with("DEP_FFMPEG_CHECK_") {
+        if name.starts_with("DEP_FFMPEG_") && !name.starts_with("DEP_FFMPEG_CHECK_") {
+            let feature_name = name["DEP_FFMPEG_".len()..name.len()].to_lowercase();
+            println!(r#"cargo::rustc-check-cfg=cfg(feature, values("{feature_name}"))"#);
+            println!(r#"cargo::rustc-cfg=feature="{feature_name}""#);
             println!(
-                r#"cargo:rustc-check-cfg=cfg(feature, values("{}"))"#,
-                name["DEP_FFMPEG_CHECK_".len()..name.len()].to_lowercase()
-            );
-        } else if name.starts_with("DEP_FFMPEG_") {
-            println!(
-                r#"cargo:rustc-cfg=feature="{}""#,
-                name["DEP_FFMPEG_".len()..name.len()].to_lowercase()
+                r#"cargo::rustc-env=DEP_FFMPEG_{}=1"#,
+                feature_name.to_uppercase()
             );
         }
     }
